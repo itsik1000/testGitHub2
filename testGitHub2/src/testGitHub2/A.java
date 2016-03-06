3d46004d5c32f3bead1c53f93b774d1c7fc2fafd
איציק <@172.16.48.52>
2016-03-06 14:19:58 +0200

    second change

--- testGitHub2/src/testGitHub2/A.java
+++ testGitHub2/src/testGitHub2/A.java
@@ -2,7 +2,7 @@ package testGitHub2;
 
 public class A 
 {
-public static void main(String[] args) {
-	System.out.println("h");
-}
+	public static void main(String[] args) {
+		System.out.println("hi how are you?");
+	}
 }
